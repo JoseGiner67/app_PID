0ec9fc20264977205e8f952a289c484398bb77b9
JoseGiner67 <52013652+JoseGiner67@users.noreply.github.com>
2023-06-08 10:04:13 +0200

    Update 2_📊_Estadísticas_de_categorías.py

--- pages/2_📊_Estadísticas_de_categorías.py
+++ pages/2_📊_Estadísticas_de_categorías.py
@@ -84,7 +84,7 @@ def display_lineplot(df,prov_name,metric):
         df_cat = df.groupby(['analytic_category','month_and_year'],as_index = False).qty_ordered.sum()
         df_cat.columns = ['Categoría','Fecha','Unidades vendidas']
         fig = px.line(df_cat, x="Fecha", y="Unidades vendidas", color="Categoría")
-        fig.update_traces(hovertemplate="Fecha: %{x|%m-%Y}<br>"
+        fig.update_traces(hovertemplate="Fecha: %{x|%B %Y}<br>"
                                 "Unidades vendidas: %{y}")
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
     
